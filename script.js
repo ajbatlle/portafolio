aee2c7c2af41f948af3b6b0b781ae327a0629c41
ajbatlle <antoniobatllel@gmail.com>
2025-06-24 17:14:38 -0400

    script.js

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 // Carrusel de imágenes por producto
 const products = [
-  [
-    "https://github.com/ajbatlle/portafolio/blob/main/img/foto2.png?raw=true",
-    "https://picsum.photos/id/1012/800/600",
-    "https://picsum.photos/id/1013/800/600"
-  ],
-  [
-    "https://picsum.photos/id/1025/800/600",
-    "https://picsum.photos/id/1026/800/600",
-    "https://picsum.photos/id/1027/800/600"
-  ],
-  [
-    "https://picsum.photos/id/1003/800/600",
-    "https://picsum.photos/id/1004/800/600",
-    "https://picsum.photos/id/1005/800/600"
-  ]
+  ["https://picsum.photos/id/1010/800/600", "https://picsum.photos/id/1011/800/600", "https://picsum.photos/id/1012/800/600"],
+  ["https://picsum.photos/id/1013/800/600", "https://picsum.photos/id/1014/800/600", "https://picsum.photos/id/1015/800/600"],
+  ["https://picsum.photos/id/1016/800/600", "https://picsum.photos/id/1017/800/600", "https://picsum.photos/id/1018/800/600"],
+  ["https://picsum.photos/id/1019/800/600", "https://picsum.photos/id/1020/800/600", "https://picsum.photos/id/1021/800/600"],
+  ["https://picsum.photos/id/1022/800/600", "https://picsum.photos/id/1023/800/600", "https://picsum.photos/id/1024/800/600"],
+  ["https://picsum.photos/id/1025/800/600", "https://picsum.photos/id/1026/800/600", "https://picsum.photos/id/1027/800/600"],
+  ["https://picsum.photos/id/1028/800/600", "https://picsum.photos/id/1029/800/600", "https://picsum.photos/id/1030/800/600"],
+  ["https://picsum.photos/id/1031/800/600", "https://picsum.photos/id/1032/800/600", "https://picsum.photos/id/1033/800/600"],
+  ["https://picsum.photos/id/1034/800/600", "https://picsum.photos/id/1035/800/600", "https://picsum.photos/id/1036/800/600"],
+  ["https://picsum.photos/id/1037/800/600", "https://picsum.photos/id/1038/800/600", "https://picsum.photos/id/1039/800/600"],
+  ["https://picsum.photos/id/1040/800/600", "https://picsum.photos/id/1041/800/600", "https://picsum.photos/id/1042/800/600"],
+  ["https://picsum.photos/id/1043/800/600", "https://picsum.photos/id/1044/800/600", "https://picsum.photos/id/1045/800/600"],
+  ["https://picsum.photos/id/1046/800/600", "https://picsum.photos/id/1047/800/600", "https://picsum.photos/id/1048/800/600"],
+  ["https://picsum.photos/id/1049/800/600", "https://picsum.photos/id/1050/800/600", "https://picsum.photos/id/1051/800/600"],
+  ["https://picsum.photos/id/1052/800/600", "https://picsum.photos/id/1053/800/600", "https://picsum.photos/id/1054/800/600"],
+  ["https://picsum.photos/id/1055/800/600", "https://picsum.photos/id/1056/800/600", "https://picsum.photos/id/1057/800/600"],
+  ["https://picsum.photos/id/1058/800/600", "https://picsum.photos/id/1059/800/600", "https://picsum.photos/id/1060/800/600"],
+  ["https://picsum.photos/id/1061/800/600", "https://picsum.photos/id/1062/800/600", "https://picsum.photos/id/1063/800/600"],
+  ["https://picsum.photos/id/1064/800/600", "https://picsum.photos/id/1065/800/600", "https://picsum.photos/id/1066/800/600"],
+  ["https://picsum.photos/id/1067/800/600", "https://picsum.photos/id/1068/800/600", "https://picsum.photos/id/1069/800/600"],
+  ["https://picsum.photos/id/1070/800/600", "https://picsum.photos/id/1071/800/600", "https://picsum.photos/id/1072/800/600"],
+  ["https://picsum.photos/id/1073/800/600", "https://picsum.photos/id/1074/800/600", "https://picsum.photos/id/1075/800/600"],
+  ["https://picsum.photos/id/1076/800/600", "https://picsum.photos/id/1077/800/600", "https://picsum.photos/id/1078/800/600"],
+  ["https://picsum.photos/id/1079/800/600", "https://picsum.photos/id/1080/800/600", "https://picsum.photos/id/1081/800/600"]
 ];
 
 let currentProduct = 0;
